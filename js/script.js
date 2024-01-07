df8e0dd3a115da87788f6b1ee4abc3b145e6263a
reham90 <rehamadel344@gmail.com>
2024-01-07 17:40:19 +0300

    edit

---
+++ js/script.js
@@ -0,0 +1,112 @@
+$(window).on('load', function() {
+  $(".preloader").fadeOut();
+
+});
+$(document).ready(function() {
+  
+
+  //phone size menu onclick
+  if ($(window).width() <= 991) {
+      $("#menu-id").click(function(e) {
+          e.preventDefault();
+          $('.menu-bars .bar').toggleClass('hide-icon');
+          $('.menu-bars .times').toggleClass('hide-icon');
+          $(".navgition").toggleClass("reset-left");
+          $("body").toggleClass("overflow");
+
+      });
+      $(".nav-head .close-btn").click(function() {
+          $(".navgition").removeClass("reset-left");
+          $(".menu-bars .bars").toggleClass("open-bars");
+          $(".menu-bars .bars").toggleClass("close-bars");
+          $("body").removeClass("overflow");
+      });
+
+
+
+
+      //slide down menu
+      $(".menu-name").click(function(e) {
+          e.preventDefault();
+          $(this).siblings(".cats-display-2").slideToggle(400);
+          $(".menu-name").not(this).siblings(".cats-display-2").slideUp(400);
+          if ($(window).width() <= 991) {
+              $(this).toggleClass("active");
+              $(".menu-name").not(this).removeClass("active");
+          }
+      });
+      $(".slide-menu-2").click(function(e) {
+          e.preventDefault();
+          $(this).siblings(".cats-dispaly-3").slideToggle(400);
+          $(".slide-menu-2").not(this).siblings(".cats-dispaly-3").slideUp(400);
+          if ($(window).width() <= 991) {
+              $(this).toggleClass("active");
+              $(".slide-menu-2").not(this).removeClass("active");
+          }
+      });
+  };
+
+  //fixed nav
+  $stickyNav = $(".top-header");
+  $(window).scroll("scroll load", function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 10) {
+          $stickyNav.addClass("fixed-nav", 500);
+      } else {
+          $stickyNav.removeClass("fixed-nav", 500);
+      }
+      if (scroll == 0) {
+          $stickyNav.removeClass("fixed-nav", 500);
+      }
+  });
+  var $stickyheader = $("header");
+  lastScroll = 0;
+  $(window).scroll("scroll load", function() {
+      var scroll = $(window).scrollTop();
+      if (lastScroll - scroll > 0) {
+          $stickyheader.addClass("fixed-header", {
+              duration: 1000
+          });
+      } else if (lastScroll - scroll >= 0 && $(window).width() <= 991) {
+          $stickyheader.addClass("fixed-header", {
+              duration: 1000
+          });
+      } else {
+          $stickyheader.removeClass("fixed-header", {
+              duration: 500
+          });
+      }
+      lastScroll = scroll;
+      if (scroll == 0) {
+          $stickyheader.removeClass("fixed-header", {
+              duration: 500
+          });
+      }
+  });
+
+  
+
+ 
+
+
+  //////////** fixed arrow to top**//////////
+  $(".arrow-top").click(function() {
+      $("html,body").animate({
+              scrollTop: 0,
+          },
+          1500
+      );
+  });
+  $(this).scrollTop() >= 500 ?
+      $(".arrow-top").fadeIn(300) :
+      $(".arrow-top").fadeOut(300);
+
+  $(window).scroll(function() {
+      $(this).scrollTop() >= 500 ?
+          $(".arrow-top").fadeIn(300) :
+          $(".arrow-top").fadeOut(300);
+  })
+
+
+
+});
